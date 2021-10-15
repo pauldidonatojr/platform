bd7166048268ae884b6d7fe438a605028acb3bda
pauldidonatojr <pjdidonatojr@gmail.com>
2021-10-15 17:36:39 -0400

    added server.js to gitignore

--- server.js
+++
@@ -1,5 +0,0 @@
-import express from "express";
-const app = express();
-const PORT = process.env.PORT || 8000;
-app.use(express.static("public"));
-app.listen(PORT, () => console.log(`listening on port ${PORT}!`));
